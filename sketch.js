776016a89568524bb792514cbb95707ed5de8625
Aditya8Prakash <60220908+Aditya8Prakash@users.noreply.github.com>
2020-03-30 17:46:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
+// made by Aditya Prakash
+
 var brush, database;
 var position;
 var brushImg;
-function preload() {
-  brushImg=loadImage("w.png");
 
+function preload() {
+  
+  brushImg=loadImage("w.png");
+  
 }
 
 function setup(){
+  
   database = firebase.database();
+  
   createCanvas(500,500);
 
   brush = createSprite(250,250,10,10);
@@ -16,11 +22,11 @@ function setup(){
 
   var brushPosition = database.ref('brush/position');
   brushPosition.on("value", readPosition, showError);
+  
 }
 
 function draw(){
 
-  
     if(keyCode === 97){
       writePosition(-1,0);
     }
@@ -33,24 +39,26 @@ function draw(){
     else if(keyCode === 115){
       writePosition(0,+1);
     }
-    drawSprites();
   
+    drawSprites();
 }
 
 function writePosition(x,y){
-  database.ref('brush/position').set({
-    'x': position.x + x ,
-    'y': position.y + y
-  })
+  
+  database.ref('brush/position').set({'x': position.x + x ,'y': position.y + y})
+  
 }
 
 function readPosition(data){
+  
   position = data.val();
+  
   brush.x = position.x;
   brush.y = position.y;
   
 }
-
 function showError(){
+  
   console.log("Error in writing to the database");
+  
 }
